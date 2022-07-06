dfc18de6305d378ec71e6f6caf24d14b60037ca6
Abhijeet Dass <78144510+Abhijeet-droid-AI@users.noreply.github.com>
2022-07-06 13:09:20 +0530

    Add files via upload

---
+++ type conversion.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int roll_no; 
+	float cgpa;
+	char gend;
+	cin>>roll_no>>cgpa>>gend;
+	cout<<roll_no<<endl;
+	cout<<cgpa<<endl;
+	cout<<gend<<endl;
+}
